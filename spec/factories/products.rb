edb04f03c5b0e9945b9abaeb51d583bcada4b71c
Jason Noble <github+jasonn@jasonnoble.org>
2013-09-02 22:48:26 -0600

    Updated stuff

---
+++ spec/factories/products.rb
@@ -0,0 +1,19 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :product do
+    name { Faker::Commerce.product_name }
+    description {
+      description = <<-DESCRIPTION
+        These #{Faker::Commerce.color} #{name.pluralize}
+        will help you generate
+        #{Faker::Company.catch_phrase} with
+        #{Faker::Company.bs}
+      DESCRIPTION
+      description.gsub!(/[\n\s]+/, ' ')
+      description
+    }
+    image_url { "/images/#{rand(10_000)}.png" }
+    price { sprintf("%.02f", rand * 999) }
+  end
+end
